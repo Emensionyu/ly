9e68b4d97c5686d6ad17227a88a70a9d7f330555
Hcy2ly <1757256226@qq.com>
2018-12-27 21:07:03 +0800

    QQ音乐

--- mango-music-chapter1 2/mango-music-chapter1 2/src/recommend/Recommend.js
+++ mango-music-chapter1 2/mango-music-chapter1 2/src/recommend/Recommend.js
@@ -4,6 +4,10 @@ import {getCarousel,getNewAlbum} from '@/api/recommend' //@直接指向src下
 import Swiper from 'swiper'
 import "swiper/dist/css/swiper.css"
 import { CODE_SUCCESS } from '../api/config';
+import * as AlbumModel from '@/model/album'
+import Loading from '@/common/loading/loading'
+import LazyLoad,{forceCheck} from 'react-lazyload'
+
 class Recommend extends Component{
     constructor(props){
         super(props);
@@ -14,26 +18,63 @@ class Recommend extends Component{
         }
     }
     componentDidMount(){
-        getCarousel().then(res => {
+        getCarousel()
+        .then(res => {
             console.log(res);
             if(res.code === CODE_SUCCESS){this.setState({
                 sliderList:res.data.slider
             },()=>{
                 if(!this.sliderSwiper) {
-                    this.sliderSwiper = new Swiper(".slider-container",{
+                    this.sliderSwiper = new 
+                    Swiper(".slider-container",{
                         loop:true,
                         autoplay:3000,
-                        pagination:'.swiper-pagination'
+                        pagination:{el:'.swiper-pagination'}
                     })
                 }
             })}
-        }).catch(err=>{
+        }).catch(err => {
             console.log(err)
+        })
+        //获取最新专辑
+        getNewAlbum()
+        .then(res => {
+            if(res){
+                if(res.code === CODE_SUCCESS){
+                    let albumList = res.albumlib.data.list;
+                    console.log(albumList);
+                    this.setState({
+                        newAlbums:albumList,
+                        loading:false
+                    },() => {
+                        // setState 数据值 同步 组件部分改变 对应的UI（模板，结点） 会被replace掉 生成新的dom结点
+                        console.log('页面专辑列表这块动态更新了');
+                    })
+                }
+            }
         })
     }
     render(){
+        let albums = this.state.newAlbums.map(item => {
+            let album = AlbumModel.createAlbumByList(item);
+            return (
+                <div className="album-wrapper" key={album.id} >
+                    <div className="left">
+                    <LazyLoad height={60}>
+                        <img src={album.img} width="100%" height="100%" alt={album.name}/>
+                    </LazyLoad>
+                        
+                    </div>
+                    <div className="right">
+                        <div className="album-name">{album.name}</div>
+                        <div className="singer-name">{album.singer}</div>
+                        <div className="public-time">{album.publicTime}</div>
+                    </div>
+                </div>
+                )
+        })
         return(
-            <div>
+            <div className="music-recommend">
                 <div className="slider-container">
                 <div className="swiper-wrapper">
                 {this.state.sliderList.map(slider=>{
@@ -44,6 +85,11 @@ class Recommend extends Component{
                     )
                 })}</div>
                 </div>
+                <div className="album-container">
+                    <h1 className="title">最新专辑</h1>
+                    <div className="album-list">{albums}</div>
+                </div>   
+                <Loading title="正在加载中..." show={this.state.loading} />
             </div>
         )
     }
